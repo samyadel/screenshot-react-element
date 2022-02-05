92ad06b0f97b888fd9a17dc13c7c6e8085bb3894
Samy Adel <samy.adel@gembitz.com>
2022-02-05 15:07:59 +0000

    first commit

---
+++ src/EditingPage/EditingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebar from './Sidebar/Sidebar';
+import Screenshot from './Screenshot/Screenshot';
+
+export default function EditingPage() {
+  return (
+    <div>
+      <Sidebar />
+      <Screenshot />
+    </div>
+  )
+}
---
+++ src/EditingPage/Screenshot/Screenshot.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import image from "./website.png"
+import "./Screenshot.css"
+
+export default function Screenshot() {
+  return <div className="gradient"><img src={image} /></div>;
+}
